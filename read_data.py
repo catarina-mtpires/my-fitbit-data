81b10f3a55d678f48f829032aff0d16a25a5dbd9
Catarina Pires <catarina.mtpires@gmail.com>
2023-05-08 20:00:13 +0200

    changed script name

--- read_data.py
+++ read_data.py
@@ -1,15 +1,14 @@
 import glob
 import json
+import datetime
 import pandas as pd
+import utils.constants as c
 
-from datetime import datetime
 
-
-hr_dir = "fitbit_data/heart/heart_rate/heart_rate-*.json"
-hr_files = glob.glob(hr_dir)
+hr_files = glob.glob(c.HR_DIR)
 
 hr_df = pd.DataFrame({"datetime": [], "bpm": [], "confidence": []})
-minute_int = 5
+minute_int = 1
 
 for f_name in hr_files:
     print(f_name)
@@ -18,10 +17,11 @@ for f_name in hr_files:
 
     dt, bpm, confidence = [], [], []
     for hr in hr_json:
-        dt_var = datetime.strptime(hr["dateTime"], "%m/%d/%y %H:%M:%S")
+        dt_var = datetime.datetime.strptime(hr["dateTime"], "%m/%d/%y %H:%M:%S")
         minute = round(dt_var.minute / minute_int) * minute_int
         if minute == 60:
-            minute -= 1
+            dt_var += datetime.timedelta(hours=1)
+            minute = 0
         dt.append(dt_var.replace(second=0, minute=minute))
         bpm.append(hr["value"]["bpm"])
         confidence.append(hr["value"]["confidence"])
@@ -30,5 +30,5 @@ for f_name in hr_files:
     frames = [hr_df, df.groupby("datetime").agg("mean").reset_index().round()]
     hr_df = pd.concat(frames)
 
-hr_df = hr_df.reset_index()
-
+hr_df = hr_df.reset_index(drop=True)
+hr_df.to_csv("fitbit_data/new_data/heart_rate.csv", index=False)
