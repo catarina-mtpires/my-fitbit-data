46224297bab35a51541c7842bbea9744bbf35ed5
Catarina Pires <catarina.mtpires@gmail.com>
2023-05-11 12:02:44 +0200

    added reading of ecg data

--- utils/pre_processing.py
+++ utils/pre_processing.py
@@ -6,6 +6,8 @@ import datetime
 import pandas as pd
 import utils.constants as c
 
+from time import strptime
+
 
 def read_json(files_dir):
     data = []
@@ -14,6 +16,15 @@ def read_json(files_dir):
         with open(f_name, 'r') as f:
             data += json.load(f)
     return data
+
+
+def read_csv(files_dir):
+    data = []
+    files = glob.glob(files_dir)
+    for f_name in files:
+        data += [pd.read_csv(f_name)]
+    data_df = pd.concat(data).reset_index(drop=True)
+    return data_df
 
 
 def create_hr_csv(minute_int=1):
@@ -74,3 +85,29 @@ def get_hr_complete_data(year, month, day):
 
     return df
 
+
+def get_dt_from_str(reading_time):
+    reading_time = reading_time.split(" ")
+    month = strptime(reading_time[1], "%b").tm_mon
+    day = reading_time[2]
+    start_time = reading_time[3]
+    year = reading_time[5]
+    dt = datetime.datetime.strptime(f"{month}/{day}/{year} {start_time}", "%m/%d/%Y %H:%M:%S")
+    return dt
+
+
+def pre_process_ecg():
+    signal_duration = 30
+    ecg_df = read_csv(c.ECG_DIR)
+    cols = ["wire_id", "heart_rate_alert", "firmware_version", "device_app_version", "hardware_version"]
+    ecg_df = ecg_df.drop(columns=cols, axis=1)
+
+    for ecg in ecg_df.iloc:
+        id = ecg.reading_id
+        dt = get_dt_from_str(ecg.reading_time)
+        signal = ecg.waveform_samples[1:-1].split("  ")
+        signal = [int(sample) for sample in signal]
+        sample_freq = len(signal)/signal_duration
+        signal_time = [dt + datetime.timedelta(seconds=i/sample_freq) for i in range(len(signal))]
+        df = pd.DataFrame({"datetime": signal_time, "value": signal})
+        df.to_csv(f"{c.NEW_HEART_DIR}ecg_{ecg.heart_rate}_{ecg.result_classification}_{id}.csv", index=False)
