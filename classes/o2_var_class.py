f85039c82aa5c1afc3ba2548c756908635656a22
Catarina Pires <catarina.mtpires@gmail.com>
2023-05-13 19:28:05 +0200

    added o2_var_class

---
+++ classes/o2_var_class.py
@@ -0,0 +1,28 @@
+import datetime
+import utils.constants as c
+from classes.base_class import DataClass
+
+
+class O2VarClass(DataClass):
+
+    def __init__(self):
+        super().__init__()
+        self.orig_dir = c.ORIG_O2_VAR_DIR
+        self.new_dir = c.O2_VAR_DIR
+        self.dt_col = "datetime"
+        self.df = self.get_df()
+
+    def create_csv(self):
+        # todo: MINUTE - ROUND TO NEAREST
+        df = self.read_csv()
+        dts = df.timestamp
+        dts_var = []
+        for dt in dts:
+            dt_var = datetime.datetime.strptime(dt, "%m/%d/%y %H:%M:%S")
+            seconds = round(dt_var.second / 60) * 60
+            if seconds == 60:
+                dt_var += datetime.timedelta(minutes=1)
+            dts_var.append(dt_var.replace(second=0))
+        df["timestamp"] = dts_var
+        df = df.rename(columns={"timestamp": "datetime", "Infrared to Red Signal Ratio": "value"})
+        df.to_csv(self.new_dir)
--- read_data.py
+++ read_data.py
@@ -1,34 +1,7 @@
-import glob
-import json
-import datetime
-import pandas as pd
-import utils.constants as c
+import classes.hr_class as hr_class
+import classes.o2_var_class as o2_var_class
 
-
-hr_files = glob.glob(c.HR_DIR)
-
-hr_df = pd.DataFrame({"datetime": [], "bpm": [], "confidence": []})
-minute_int = 1
-
-for f_name in hr_files:
-    print(f_name)
-    with open(f_name, 'r') as f:
-        hr_json = json.load(f)
-
-    dt, bpm, confidence = [], [], []
-    for hr in hr_json:
-        dt_var = datetime.datetime.strptime(hr["dateTime"], "%m/%d/%y %H:%M:%S")
-        minute = round(dt_var.minute / minute_int) * minute_int
-        if minute == 60:
-            dt_var += datetime.timedelta(hours=1)
-            minute = 0
-        dt.append(dt_var.replace(second=0, minute=minute))
-        bpm.append(hr["value"]["bpm"])
-        confidence.append(hr["value"]["confidence"])
-
-    df = pd.DataFrame({"datetime": dt, "bpm": bpm, "confidence": confidence})
-    frames = [hr_df, df.groupby("datetime").agg("mean").reset_index().round()]
-    hr_df = pd.concat(frames)
-
-hr_df = hr_df.reset_index(drop=True)
-hr_df.to_csv("fitbit_data/new_data/heart_rate.csv", index=False)
+hr = hr_class.HeartRateClass()
+rhr = hr_class.RestingHeartRateClass()
+ecg = hr_class.ECGClass()
+o2_var = o2_var_class.O2VarClass()
